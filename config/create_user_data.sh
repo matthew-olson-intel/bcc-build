fb4611bf3cd4117d16af708767fcae1994668e2b
Ben Olson <matthew.olson@intel.com>
2023-10-11 21:09:39 -0500

    Cleaning up binary 'uploader'

--- config/create_user_data.sh
+++ config/create_user_data.sh
@@ -7,24 +7,44 @@
 # a `write_files` block to the `cloud-init` config.
 #
 # WARNING: This script ASSUMES that your `user-data.yaml.in` ENDS in a `write_files`
-# block, and appends more files to that. You cannot have multiple `write_files` blocks
+# block, and appends more files to that. You cannot have multiple `write_files` blocks.
 ################################################################################
 BASEDIR=$(cd "${BASH_SOURCE[0]%/*}" && pwd)
 
 CONFIG_DIR="${BASEDIR}"
 DATA_DIR="${CONFIG_DIR}/../data"
 USER_DATA="${CONFIG_DIR}/user-data.yaml"
+ADDL_USER_DATA="${CONFIG_DIR}/addl-user-data.yaml"
 
 cp ${CONFIG_DIR}/user-data.yaml.in ${USER_DATA}
 
-for file in $(ls ${DATA_DIR}); do
+# If there's additional user data, go ahead and append it now.
+# We shouldn't append it later, because if it contains
+# any write_files directives, we want to append lines to that,
+# since there can't be multiple.
+if [ -f ${ADDL_USER_DATA} ]; then
+  cat ${ADDL_USER_DATA} >> ${USER_DATA}
+fi
 
-  echo "  -   encoding: gzip" >>${USER_DATA}
-  echo "      path: /home/ubuntu/${file}" >>${USER_DATA}
-  echo "      content: !!binary |" >>${USER_DATA}
-  echo -n "        " >>${USER_DATA}
-  cat ${DATA_DIR}/${file} | gzip | base64 -w0 >>${CONFIG_DIR}/user-data.yaml
-  
+# Get an array of filenames in $DATA_DIR
+shopt -s nullglob
+data_files=(${DATA_DIR}/*)
+if [ ${#data_files[@]} -eq 0 ]; then
+  exit 0
+fi
+
+# Determine if there are any write_files directives
+if ! grep -xq "write_files:" ${USER_DATA}; then
+  echo "write_files:" >>${USER_DATA}
+fi
+
+for file in $(ls ${DATA_DIR}); do
+  data=$(cat ${DATA_DIR}/${file} | gzip | base64 -w0)
+  text+="  -  encoding: gzip\n"
+  text+="     path: /home/ubuntu/${file}\n"
+  text+="     content: !!binary |\n"
+  text+="       ${data}"
+  printf "%b" "${text}" >>${USER_DATA}
 done
 
 cloud-localds ${CONFIG_DIR}/seed.img ${CONFIG_DIR}/user-data.yaml ${CONFIG_DIR}/metadata.yaml
